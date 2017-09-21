9eb134c5a6b04d7aead25298089ec155acca4593
Reifier <nwe.apinchuk@us.westfield.com>
2017-09-21 16:57:16 -0700

    More stuff

--- update_api_scopes.sh
+++ update_api_scopes.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
-ZONE="us-west1-a"
+
+ZONE=$(gcloud container clusters list | grep -i environ | tr -s ' ' | cut -d ' ' -f 2)
 CLUSTER_NAME=$(gcloud container clusters list --zone=${ZONE} | grep -i environment | cut -d ' ' -f 1)
-NUMBER_OF_NODES=$(gcloud container clusters list --zone=${ZONE} | grep -i environment | cut -d ' ' -f 1)
-MACHINE_TYPE=
-
-gcloud container clusters list --zone=${ZONE}
-
+NUMBER_OF_NODES=$(gcloud container clusters list --zone=us-west1-a | grep -i environment | tr -s ' ' | cut -d ' ' -f 7)
+MACHINE_TYPE=$(gcloud container clusters list --zone=us-west1-a | grep -i environment | tr -s ' ' | cut -d ' ' -f 5)
+DEFAULT_POOL_NODES=$(kubectl get nodes | grep -i default-pool | cut -d ' ' -f 1)
 
 
+gcloud container node-pools create nodepool01 --cluster ${CLUSTER_NAME} --zone ${ZONE} --num-nodes ${NUMBER_OF_NODES} --scopes https://www.googleapis.com/auth/devstorage.read_write,https://www.googleapis.com/auth/pubsub,https://www.googleapis.com/auth/monitoring --machine-type ${MACHINE_TYPE}
+
+for node in $DEFAULT_POOL_NODES; do kubectl cordon $node; done
+for node in $DEFAULT_POOL_NODES;do kubectl drain $node --force; done
+
+gcloud container node-pools delete default-pool --zone=$ZONE --cluster=$CLUSTER_NAME
